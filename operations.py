c63d313f924ccb0baa1aa394b587650ed962dc23
Scott Little <swlittle7@gmail.com>
2020-09-19 14:38:48 -0500

    Comment it

--- operations.py
+++ operations.py
@@ -3,6 +3,7 @@ import os
 
 
 def add_config(app, file, contents):
+    # Basic way to add an app config
     with sqlite3.connect(os.getenv('HOME') + '/.dotfiles.db') as db:
         cur = db.cursor()
         cur.execute("INSERT INTO dotfiles VALUES (?, ?, ?)", (app, file, contents))
@@ -11,14 +12,16 @@ def add_config(app, file, contents):
 
 
 def update_file(file, contents):
+    # Update config of file -- ignore app
     with sqlite3.connect(os.getenv('HOME') + '/.dotfiles.db') as db:
         cur = db.cursor()
-        cur.execute("UPDATE dotfiles SET contents=? WHERE file=?",( file, contents))
+        cur.execute("UPDATE dotfiles SET contents=? WHERE file=?", (file, contents))
         db.commit()
         cur.close()
 
 
 def get_file(file):
+    # Get config of file, no app
     with sqlite3.connect(os.getenv('HOME') + '/.dotfiles.db') as db:
         cur = db.cursor()
         cur.execute('SELECT contents FROM dotfiles WHERE file=?', (file,))
@@ -29,12 +32,14 @@ def get_file(file):
 
 
 def delete_file(file):
+    # remove config file
     with sqlite3.connect(os.getenv('HOME') + '/.dotfiles.db') as db:
         cur = db.cursor()
         cur.execute("DELETE FROM dotfiles WHERE file=?", (file,))
 
 
 def get_app_config(app):
+    # Get all filenames and content of app
     with sqlite3.connect(os.getenv('HOME') + '/.dotfiles.db') as db:
         cur = db.cursor()
         cur.execute("SELECT file, contents FROM dotfiles WHERE app=?", (app,))
